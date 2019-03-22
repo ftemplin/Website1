da9c00b3b0ad759ef5693502bd5acb7634123d0b
Fritz Templin <fritz.templin@gmail.com>
2019-03-22 09:40:01 -0700

    Add files via upload

---
+++ website1.js
@@ -0,0 +1,8 @@
+//alert("Connected to Website1");
+
+var firstInput = document.querySelector("li");
+
+firstInput.addEventListener("mouseover", function(){
+	// console.log("mouse over");
+	firstInput.style.color = "green";
+});
